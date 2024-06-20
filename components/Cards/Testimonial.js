6e2908992ce99a1f20ab63fe915d876974fe9d65
ProScriptSlinger <stanely.davis.941105@gmail.com>
2024-06-21 04:38:52 +0900

    fix: remove starts

--- components/Cards/Testimonial.js
+++ components/Cards/Testimonial.js
@@ -1,34 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Paper from '@mui/material/Paper';
-import Avatar from '@mui/material/Avatar';
-import Typography from '@mui/material/Typography';
-import StarIcon from '@mui/icons-material/Star';
-import useStyles from './cards-style';
+import React from "react";
+import PropTypes from "prop-types";
+import Paper from "@mui/material/Paper";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+import StarIcon from "@mui/icons-material/Star";
+import useStyles from "./cards-style";
 
 function Testimonial(props) {
   const { classes } = useStyles();
-  const {
-    avatar,
-    title,
-    name,
-    text,
-    star,
-  } = props;
+  const { avatar, title, name, text, star } = props;
   return (
     <div className={classes.testiCard}>
       <Paper className={classes.paper}>
         <Typography display="block" component="p">
           {text}
         </Typography>
-        <div className={classes.rating}>
+        {/* <div className={classes.rating}>
           {[...Array(star)].map((e, index) => (
             <StarIcon className={classes.starIcon} key={index.toString()} />
           ))}
           {[...Array(5 - star)].map((e, index) => (
             <StarIcon className={classes.starIconDisable} key={index.toString()} />
           ))}
-        </div>
+        </div> */}
       </Paper>
       <div className={classes.person}>
         <Avatar alt={name} src={avatar} className={classes.avatar} />
